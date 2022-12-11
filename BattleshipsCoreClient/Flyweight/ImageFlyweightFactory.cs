554049265a41122617c19748f7f07b9ee7a4beb0
ArnasTam <arnas.tamasauskas4@gmail.com>
2022-12-11 20:16:25 +0200

    Flyweight

---
+++ BattleshipsCoreClient/Flyweight/ImageFlyweightFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipsCoreClient.Flyweight
+{
+    public class ImageFlyweightFactory
+    {
+        private Dictionary<string, ImageFlyweight> flyweights { get; set; } = new Dictionary<string, ImageFlyweight>();
+
+        public ImageFlyweightFactory()
+        {
+            flyweights.Add("ship", new ConcreteImageFlyweight(Image.FromFile("..\\..\\..\\Assets\\ship.png")));
+
+        }
+        public ImageFlyweight GetFlyweight(string key)
+        {
+            return ((ImageFlyweight)flyweights[key]);
+        }
+    }
+}
